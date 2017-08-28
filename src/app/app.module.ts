248948c9f0b274ee231018b0a3b40cfaccff7b56
mookylicious <adrienguyot17@gmail.com>
2017-08-28 16:34:08 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { HikeModule } from './hike/hike.module';
+import { HomeModule } from './home/home.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HikeListComponent } from './hike/hike-list.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { HikeDetailsComponent } from './hike/hike-details.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+
+    HikeModule,
+    HttpModule,
+    HomeModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'hikes', component: HikeListComponent },
+      { path: 'hikes/:id', component: HikeDetailsComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/hike/hike.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HikeListComponent } from './hike-list.component';
+import { HikeDetailsComponent } from './hike-details.component';
+import { HikeService } from './hike.service';
+
+@NgModule({
+    imports: [CommonModule, RouterModule],
+    declarations: [HikeListComponent, HikeDetailsComponent],
+    exports: [HikeListComponent],
+    providers: [HikeService]
+})
+
+export class HikeModule { }
