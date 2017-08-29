734b1eb92514853a45d6dbd2883ba856b6850f62
mookylicious <adrienguyot17@gmail.com>
2017-08-29 14:24:00 +0200

    Gestion de l'évènement de soumission du formulaire

--- src/app/contact/contact-us.component.ts
+++ src/app/contact/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
 export class ContactUsComponent {
     subject: string;
     isVisible: boolean = false;
+
+    sendMessage(form: NgForm) {
+        console.log(form.value);
+    }
 }
