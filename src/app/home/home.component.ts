763832b1f18169cd55985e9363e7f8ce246cc2c8
mookylicious <adrienguyot17@gmail.com>
2017-08-30 12:14:33 +0200

    Animation de la home page avec affichage/masquage d'un bloc

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, animate, style, transition, keyframes } from '@angular/core';
 
 
 @Component({
     moduleId: module.id,
+    animations: [
+        trigger('toggleElement', [
+            state('open', style({ 'height': '*'})),
+            state('closed', style({ 'height': '0px', 'font-size': '0px'})),
+
+            transition('closed <=> open', [
+                animate(1000)
+            ])
+        ])
+    ],
     selector: 'home',
     templateUrl: 'home.component.html'
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+    open: boolean = false;
+    toggleElement: string;
+
+    toggle(){
+        this.open = !this.open;
+        if (this.open) {
+            this.toggleElement = 'open';
+        } else {
+            this.toggleElement = 'closed';
+        }
+    }
+}
